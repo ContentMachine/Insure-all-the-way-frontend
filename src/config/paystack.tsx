0ff62e02c608b6a9f4ef7be89119aa908bdd0030
tobe_wdw <softwaredeveloper@wdwltd.com>
2025-03-10 18:05:32 +0100

    Added the agent auth

--- src/config/paystack.tsx
+++ src/config/paystack.tsx
@@ -1,4 +1,4 @@
 export const PAYSTACK_PUBLIC_KEY =
   process?.env?.NODE_ENV === "development"
     ? process?.env?.NEXT_PUBLIC_PAYSTACK_PUBLIC_KEY
-    : process?.env?.NEXT_PUBLIC_PAYSTACK_PUBLIC_LIVE_KEY;
+    : process?.env?.NEXT_PUBLIC_PAYSTACK_PUBLIC_KEY;
